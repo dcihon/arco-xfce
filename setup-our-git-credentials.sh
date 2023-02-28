0bfdc55d00206387f210dde5bf72dba0775cd346
dcihon <danc@dccathome.com>
2023-02-28 08:15:52 -0500

    first commit

--- setup-our-git-credentials.sh
+++ setup-our-git-credentials.sh
@@ -62,13 +62,13 @@ echo "--------------------------------------------------------------------------
 
 git config --global pull.rebase false
 git config --global push.default simple
-git config --global user.name "arcolinuxz"
-git config --global user.email "arcolinuxinfo@gmail.com"
+git config --global user.name "dcihon"
+git config --global user.email "danc@dccathome.com"
 sudo git config --system core.editor nano
 #git config --global credential.helper cache
 #git config --global credential.helper 'cache --timeout=32000'
 
-git remote set-url origin git@github.com-arc:$githubdir/$project
+git remote set-url origin git@github.com:$githubdir/$project
 
 echo
 tput setaf 3
